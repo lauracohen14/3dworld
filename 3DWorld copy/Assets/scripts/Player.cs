f9e568b1e2235e37e965498916f243904d28513c
Laura Cohen <lauramimona@gmail.com>
2018-07-12 17:29:11 +0200

    first commit
    first commity, 3rd person camera player movement and some objects working

---
+++ 3DWorld copy/Assets/scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveControl))]
+public class Player : MonoBehaviour {
+
+
+	[System.Serializable]
+	public class mInput{
+		public Vector2 Damping;
+		public Vector2 Sensitivty;
+	
+	}
+	[SerializeField]float speed;
+	[SerializeField]mInput mouseControl;
+
+	private MoveControl moveC;
+	public MoveControl MoveControl {
+		get{ 
+			if (moveC == null) {
+				moveC = GetComponent<MoveControl> ();
+			}
+				
+			return moveC;
+		}
+	}
+
+
+
+
+	void Awake () {
+		Manager.Instance.LocalPlayer = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = new Vector2 (Manager.Instance.InputControl.Vertical * speed, Manager.Instance.InputControl.Horizontal * speed);
+		MoveControl.Move(direction);
+	}
+
+}
---
+++ 3DWorld copy/Assets/scripts/thirdPerson.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPerson : MonoBehaviour {
+	
+	[SerializeField]Vector3 cameraOffset;
+	[SerializeField]float damping;
+
+
+	Transform Ahead;
+
+	public Player localPlayer;
+	// Use this for initialization
+	void Awake () {
+		Manager.Instance.playerJoined += HandleOnPlayerJoined;;
+	}
+
+	void HandleOnPlayerJoined(Player player){
+		localPlayer = player;
+		Ahead = localPlayer.transform.Find ("Ahead");
+
+		if (Ahead == null)
+			Ahead = localPlayer.transform;
+	}
+	
+	// Update is called once per frame
+	//Sets target position and adds transforms in front of player then moves camera behind player
+	void Update () {
+		Vector3 targPos = Ahead.position + localPlayer.transform.forward * cameraOffset.z
+		                  + localPlayer.transform.up * cameraOffset.y +
+		                  localPlayer.transform.right * cameraOffset.x;
+
+		transform.position = Vector3.Lerp (transform.position, targPos, damping + Time.deltaTime);
+	}
+}
